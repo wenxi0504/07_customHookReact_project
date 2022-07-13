4ebbdd6737ef1c6026f92492ef7d6e72ffc9a8d5
Sophie <wenxi1988@hotmail.com>
2022-07-12 21:41:11 -0400

    custom-use hook

--- .history/src/hooks/use-counter_20220712212303.js
+++ .history/src/hooks/use-counter_20220712212303.js
@@ -1,19 +1,15 @@
 import { useState, useEffect } from "react";
 
-const useCounter = (forwards = true) => {
+const useCounter = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (forwards) {
-        setCounter((prevCounter) => prevCounter + 1);
-      } else {
-        setCounter((prevCounter) => prevCounter - 1);
-      }
+      setCounter((prevCounter) => prevCounter + 1);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [forwards]);
+  }, []);
 
   return counter;
 };
--- .history/src/hooks/use-counter_20220712213448.js
+++ .history/src/hooks/use-counter_20220712213448.js
@@ -1,19 +1,15 @@
 import { useState, useEffect } from "react";
 
-const useCounter = (forwards = true) => {
+const useCounter = (counterUpdatedFn) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (forwards) {
-        setCounter((prevCounter) => prevCounter + 1);
-      } else {
-        setCounter((prevCounter) => prevCounter - 1);
-      }
+      setCounter(counterUpdatedFn());
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [forwards]);
+  }, []);
 
   return counter;
 };
